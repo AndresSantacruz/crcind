54fa2e3fb1f72f5809c3fee5d6bc57ba880c0b1c
gio <giojimarc@gmail.com>
2021-02-18 16:55:27 -0500

    Cambio

--- GetCrcind.go
+++ GetCrcind.go
@@ -1,22 +1,20 @@
 package crcind
 
 import (
-	"io/ioutil"
+	"bytes"
 	"log"
 	"net/http"
+
+	xj "github.com/basgys/goxml2json"
 )
 
-func GetCrcind(query string) []byte {
+func GetCrcind(query string) *bytes.Buffer {
 	res, err := http.Get("http://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=GetByName&name=" + query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+	json, _ := xj.Convert(res.Body)
 
-	return data
+	return json
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/AndresSantacruz/crcind
 
 go 1.13
+
+require (
+	github.com/basgys/goxml2json v1.1.0
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
